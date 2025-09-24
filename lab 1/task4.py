51c807371b68a74f14b48e008038402ab53ad89c
Mostafa Mahmoud <mostafa2742002mahmoud@gmail.com>
2025-09-24 11:56:49 +0300

    Fix area calculation precision in circle area formula

--- lab 1/task4.py
+++ lab 1/task4.py
@@ -1,6 +1,6 @@
 n = input("Enter The radius of the circle : ")
 radius = float(n)
-area = 3.14159 * radius ** 2
+area = 3.14 * radius ** 2
 circumference = 2 * 3.14159 * radius
 print("Area of the circle:", area)
 print("Circumference of the circle:", circumference)
