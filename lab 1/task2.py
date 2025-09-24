2020acab0ba87f62c6fa639e646c16e6553d33c5
Mostafa Mahmoud <mostafa2742002mahmoud@gmail.com>
2025-09-24 13:34:36 +0300

    Enhance binary number validation by checking for invalid digits

--- lab 1/task2.py
+++ lab 1/task2.py
@@ -1,9 +1,20 @@
+nums = ['2','3','4','5','6','7','8','9']
 while True:
     n = input("Enter Your Binary Number : ")
+    flag = 0
     if n.isalpha():
         print("invalid Binary Number")
         continue
     else :
+        for i in nums:
+            if n.count(i):
+                flag = 1
+                
+    if flag:
+        print("invalid Binary Number")
+        continue
+    else :
         break
+
 
 print("The Decimal Representation is : ",int(n,2))
