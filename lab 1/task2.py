651437e21b56f2db1f6194ba2cb6bb5d5bf37b1c
Mostafa Mahmoud <mostafa2742002mahmoud@gmail.com>
2025-09-24 13:18:02 +0300

    Add input validation for binary number and radius in circle calculations

--- lab 1/task2.py
+++ lab 1/task2.py
@@ -1,2 +1,5 @@
 n = input("Enter Your Binary Number : ")
+if n.isalpha():
+    print("un valid Binary Number")
+    exit()
 print("The Decimal Representation is : ",int(n,2))
--- lab 1/task4.py
+++ lab 1/task4.py
@@ -1,4 +1,7 @@
 n = input("Enter The radius of the circle : ")
+if n.isalpha():
+    print("Not Valid Num")
+    exit()
 radius = int(n)
 area = 3.14 * radius ** 2
 circumference = 2 * 3.14159 * radius
