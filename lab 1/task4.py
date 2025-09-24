11be9c0721963df95ca898c9bb53f31a99077d61
Mostafa Mahmoud <mostafa2742002mahmoud@gmail.com>
2025-09-24 11:57:13 +0300

    Fix radius input type from float to int for area calculation

--- lab 1/task4.py
+++ lab 1/task4.py
@@ -1,5 +1,5 @@
 n = input("Enter The radius of the circle : ")
-radius = float(n)
+radius = int(n)
 area = 3.14 * radius ** 2
 circumference = 2 * 3.14159 * radius
 print("Area of the circle:", area)
